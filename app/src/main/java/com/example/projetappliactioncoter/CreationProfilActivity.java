cbd6a8a626fefbc64683729e5ffdc7e99c311060
miska1234 <miska.r.h.tr@gmail.com>
2022-03-28 15:07:39 -0400

    Ajoute des classe relié à l'interface : -Acceuile -Connection -Cote R -Création profil -Enregistrement -Logo -Menu -Profil -Université

---
+++ app/src/main/java/com/example/projetappliactioncoter/CreationProfilActivity.java
@@ -0,0 +1,14 @@
+package com.example.projetappliactioncoter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreationProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_profil);
+    }
+}
