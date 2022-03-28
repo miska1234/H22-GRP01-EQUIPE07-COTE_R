eb704961d73426dc462336ffd0208d564caac510
Kameleonn <71809290+Kameleonn@users.noreply.github.com>
2022-03-28 19:49:10 -0400

    -Creation du build gradle qui permet la base de donnée de FireBase -Creation d’une activité d’enregistrementV2

--- app/src/main/java/com/example/projetappliactioncoter/AcceuilleActivity.java
+++ app/src/main/java/com/example/projetappliactioncoter/AcceuilleActivity.java
@@ -2,13 +2,39 @@ package com.example.projetappliactioncoter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AcceuilleActivity extends AppCompatActivity {
+public class AcceuilleActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bouttonEnregistrer, boutonConnecter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceuille);
+
+        boutonConnecter = findViewById(R.id.buttonAllerSeConnecter);
+        bouttonEnregistrer = findViewById(R.id.buttonAllerSenregistrer);
+
+        boutonConnecter.setOnClickListener(this);
+        bouttonEnregistrer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.buttonAllerSeConnecter:
+                Intent intentConnecter = new Intent(AcceuilleActivity.this, ConnectionActivity.class);
+                startActivity(intentConnecter);
+                break;
+            case R.id.buttonAllerSenregistrer:
+                Intent intentEnregistrer = new Intent(AcceuilleActivity.this, EnregistrementV2Activity.class);
+                startActivity(intentEnregistrer);
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/projetappliactioncoter/EnregistrementV2Activity.java
@@ -0,0 +1,14 @@
+package com.example.projetappliactioncoter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnregistrementV2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enregistrement_v2);
+    }
+}
