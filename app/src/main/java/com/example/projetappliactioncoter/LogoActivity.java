4868eb9e02b778b6ab8cb01007dfb96b05247774
Kameleonn <71809290+Kameleonn@users.noreply.github.com>
2022-03-28 18:43:29 -0400

    Timer pour la premiere activity

--- app/src/main/java/com/example/projetappliactioncoter/LogoActivity.java
+++ app/src/main/java/com/example/projetappliactioncoter/LogoActivity.java
@@ -2,13 +2,28 @@ package com.example.projetappliactioncoter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class LogoActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LogoActivity.this, AcceuilleActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }
