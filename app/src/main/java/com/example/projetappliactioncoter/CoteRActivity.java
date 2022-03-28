4343dbe59d62b49b4cf2af21da91b9c372bbfaa1
Kameleonn <71809290+Kameleonn@users.noreply.github.com>
2022-03-28 16:52:28 -0400

    Test commit yusuf

--- app/src/main/java/com/example/projetappliactioncoter/CoteRActivity.java
+++ app/src/main/java/com/example/projetappliactioncoter/CoteRActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CoteRActivity extends AppCompatActivity {
-
+//TEST COMMIT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
