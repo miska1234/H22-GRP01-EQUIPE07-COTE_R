0f45b175e53fabf093748a51a4e2ff3b52631b2d
mahdi <mahdi_djell@outlook.com>
2022-04-25 15:09:51 -0400

    version finale du calcul

--- app/src/main/java/com/example/projetappliactioncoter/CalculCoteR.java
+++ app/src/main/java/com/example/projetappliactioncoter/CalculCoteR.java
@@ -1,109 +1,77 @@
 package com.example.projetappliactioncoter;
 
 
-
 import java.util.Scanner;
 
-    public class CalculCoteR {
+public class CalculCoteR {
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            double note = note(sc);
-            double moyenne = moyenne(sc);
-            int cotez = cotez(sc);
-            int ifgz = ifgz(sc);
-            int idgz = idgz(sc);
-            float coter = calculfinal(ifgz, idgz, cotez);
-            System.out.println("Cote R finale: " + coter);
-        }
-        public static float calculfinal(int ifgz, int idgz, int cotez){
-            float coter = 0;
-            coter = (cotez * idgz + ifgz) * 5;
+    public static void main(String[] args) {
 
-            return coter;
-        }
-
-        public static double ajouter( double unitecour, double unitetotal, int nbdecours){
-            double w = unitecour/unitetotal;
-            return w;
-        }
-        public static double unite(Scanner sc){
-            double unite = -100;
-            boolean valid = false;
-            while (!valid){
-                System.out.println("Quelle est votre note?");
-                unite = sc.nextDouble();
-                if (unite >= 0 && unite <= 3){
-                    valid = true;
-                }
-            }
-            return unite;
-        }
-
-        public static double note(Scanner sc){
-            double note = -100;
-            boolean valid = false;
-            while (!valid){
-                System.out.println("Quelle est votre note?");
-                note = sc.nextDouble();
-                if (note >= 0 && note <= 100){
-                    valid = true;
-                }
-            }
-            return note;
-        }
-        public static double moyenne(Scanner sc){
-            double moyenne = -100;
-            boolean valid = false;
-            while (!valid){
-                System.out.println("Quelle est la moyenne de votre groupe?");
-                moyenne = sc.nextDouble();
-                if (moyenne >= 0 && moyenne <= 100){
-                    valid = true;
-                }
-            }
-            return moyenne;
-        }
-        public static int cotez(Scanner sc){
-
-            //cote z de -4 à 4
-            boolean valid = false;
-            int cotez = -100;
-            while (!valid){
-                System.out.println("Choisissez une cote z partant de -4 (très mauvais) à 4 (très bien):");
-                cotez = sc.nextInt();
-                if (cotez <= 4 && cotez >= -4)
-                    valid = true;
-            }
-            return cotez;
-        }
-        public static int ifgz(Scanner sc){
-
-            //ifgz de -3 à 3
-            boolean valid = false;
-            int ifgz = -100;
-            while (!valid){
-                System.out.println("Choisissez un indice de force de groupe (IFGZ) partant de -3 (très mauvais) à 3 (très bien):");
-                ifgz = sc.nextInt();
-                if (ifgz <= 3 && ifgz >= -3)
-                    valid = true;
-            }
-            return ifgz;
-        }
-        public static int idgz(Scanner sc){
-
-            //idgz de -3 à 3
-            boolean valid = false;
-            int idgz = -100;
-            while (!valid){
-                System.out.println("Choisissez un écart type de la cote z partant de -3 (très mauvais) à 3 (très bien):");
-                idgz = sc.nextInt();
-                if (idgz <= 3 && idgz >= -3)
-                    valid = true;
-            }
-            return idgz;
-        }
-
-
+        double cotez = cotez();
+        double ifgz = ifgz();
+        System.out.println("Voici votre Cote R approximative : " + coter(ifgz, cotez));
     }
+    public static double coter(double ifgz, double cotez){
+        double coter;
+        coter = (cotez + ifgz + 5) * 5;
+
+        return coter;
+    }
+
+    public static double cotez(){
+        Scanner sc = new Scanner(System.in);
+        Scanner scD = new Scanner(System.in);
+        double cotez ;
+        int note = 0;
+        boolean notevalide = false;
+        double ecartype = 0;
+        boolean ecartypevalide = false;
+        int moyenne = 0;
+        boolean moyennevalide = false;
+
+        while (!notevalide){
+            System.out.println("Quelle est votre note en % (entre 0 et 100) : ");
+            note = sc.nextInt();
+            if (note <= 100 && note >= 0)
+                notevalide = true;
+        }
+
+        while (!moyennevalide){
+            System.out.println("Quelle est le moyenne du groupe en % (entre 0 et 100) : ");
+            moyenne = sc.nextInt();
+            if (moyenne <= 100 && moyenne >= 0)
+                moyennevalide = true;
+        }
+        while (!ecartypevalide){
+            System.out.println("Quelle est l'écart type du groupe en % (entre 0 et 100) : ");
+            ecartype = scD.nextDouble();
+            if (ecartype <= 100 && ecartype >= 0)
+                ecartypevalide = true;
+        }
+
+        cotez = (note - moyenne)/ecartype;
+
+        return cotez;
+    }
+
+    public static double ifgz(){
+        Scanner sc = new Scanner(System.in);
+        double ifgz ;
+        double moyenne = 0;
+        boolean moyennevalide = false;
+
+        while (!moyennevalide){
+            System.out.println("Quelle était votre la moyenne de votre groupe au secondaire en % (** mettez 75 si vous ne savez pas **)");
+            moyenne = sc.nextDouble();
+            if (moyenne <= 100 && moyenne >= 0)
+                moyennevalide = true;
+        }
+        ifgz = (moyenne - 75) / 14;
+        return ifgz;
+    }
+
+
+}
+
+
 
